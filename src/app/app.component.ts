19e0d55423bcf2f64edb52b2d01e0ed525942644
Binbogamee <angrylamb1@gmail.com>
2021-05-17 17:16:09 +0300

    last v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,44 +7,47 @@ import { HttpMynotesService } from './shared/services/http-mynotes.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'project14';
 
   notes: Note[];
+  flag = true;
 
-  constructor(private HttpMynotesService: HttpMynotesService) {}
-  
-ngOnInit() {
-  this.getData();
-}
+  constructor(private HttpMynotesService: HttpMynotesService) { }
 
-  note: Note = {id: 0, name: '', text: '', date: new Date()};
+  ngOnInit() {
+    this.getData();
+  }
+
+  note: Note = { id: 0, name: '', text: '', date: new Date() };
   edit = false;
 
   async getData() {
     try {
       this.notes = await this.HttpMynotesService.getNotes();
     } catch (error) {
+      this.flag = false;
       console.log(error);
+
     }
   }
 
-async AddNote(note: Note) {
-  try {
-    let newid = this.notes.findIndex(elm => elm.id == note.id);
-    if (newid != -1) {
-      await this.HttpMynotesService.patchNote(note);
-    }
-    else if (newid == -1) {
-      await this.HttpMynotesService.postNote(note);
-    }
-    this.edit = false;
-      } catch (error) {
-        console.log(error);
-      } finally {
-        this.getData();
+  async AddNote(note: Note) {
+    try {
+      let newid = this.notes.findIndex(elm => elm.id == note.id);
+      if (newid != -1) {
+        await this.HttpMynotesService.patchNote(note);
       }
-}
+      else if (newid == -1) {
+        await this.HttpMynotesService.postNote(note);
+      }
+      this.edit = false;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.getData();
+    }
+  }
 
   async onDeleteNote(id: number) {
     try {
@@ -57,7 +60,7 @@ async AddNote(note: Note) {
   }
 
   onEditNote(id: number) {
-    let idx=this.notes.findIndex(elm => elm.id == id);
+    let idx = this.notes.findIndex(elm => elm.id == id);
     this.note = this.notes[idx];
     this.edit = true;
   }
